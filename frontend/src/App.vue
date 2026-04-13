e21357963e5cda63533bdf53b6af1b91dcf1e68f
0xWulf <zk@hexawulf.dev>
2026-04-13 17:06:21 +0800

    feat(frontend): Coding Day 1: Created a layout in App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,8 +1,27 @@
 <script setup>
+
 </script>
 
 <template>
   <div id="app">
-    
+    <div class="bg-primary text-white p-3">
+      NavBar placeholder
+    </div>
+
+
+    <div class="d-flex">
+
+      <aside
+        class="d-none d-md-block bg-light border-end p-3"
+        style="width: 240px; min-height: 90vh;"
+      >
+        Sidebar placeholder
+      </aside>
+
+
+      <main class="flex-grow-1 bg-body p-3" style="min-height: 90vh;">
+        Main content placeholder
+      </main>
+    </div>
   </div>
 </template>
